9af5b2d4fc4d6a5e8d3997183a4afc235ab013dc
Insidiae <insidiae423@gmail.com>
2018-11-07 02:41:03 +0800

    Finish Module 3: Server Side GraphQL

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,12 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+  items: forwardTo('db')
+  // equivalent to this code below; particularly helpful if there's no custom logic for your query
+  // async items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // }
+};
 
 module.exports = Query;
